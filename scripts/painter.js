9452f26113a24d62144a04a9753622b38a9a8271
= <=>
2011-06-09 21:20:31 -0600

    Finished the basic control form.

--- scripts/painter.js
+++ scripts/painter.js
@@ -1,6 +1,8 @@
 
 PIXELSPERINCH = 8
 
+WIDTH = 133.5*PIXELSPERINCH + 2
+HEIGHT = 94*PIXELSPERINCH + 2
 
 $(function(){
   Painter.init();
@@ -28,13 +30,23 @@ Painter = {
     $("#resetbutton").button().click(Painter.reset);
     
     Painter.setupColorPicker('#color1');
-    
-    
+		Painter.setupColorPicker('#color2');
+		Painter.setupColorPicker('#color3');
+		Painter.setupColorPicker('#color4');
   },
   
   setupColorPicker: function(id) {
+		var cl = '#0000ff'
+		
+		if($.cookie(id)) {
+			cl = "#" + $.cookie(id);
+		}
+		
+		$(id + " div").css('backgroundColor', cl);
+		
+	
     $(id).ColorPicker({
-    	color: '#0000ff',
+    	color: cl,
     	onShow: function (colpkr) {
     		$(colpkr).fadeIn(500);
     		return false;
@@ -45,15 +57,17 @@ Painter = {
     	},
     	onChange: function (hsb, hex, rgb) {
     		$(id + ' div').css('backgroundColor', '#' + hex);
+				$.cookie(id, hex)
     	}
     });
   },
   
   start: function() {
-    
-    
     $("#setup").slideUp()
     $("#reset").slideDown();
+
+		$("#mainCanvas").css("height", HEIGHT)
+		$("#mainCanvas").css("width", WIDTH)
   },
   
   reset: function() {
